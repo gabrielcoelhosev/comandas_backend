86679bb2484be74158fee340f71fa98925286801
gabrielcoelhosev <gabrielcoelhosev.dev@gmail.com>
2026-03-21 17:15:35 -0300

    feat: sinc to async

--- src/routers/FuncionarioRouter.py
+++ src/routers/FuncionarioRouter.py
@@ -6,21 +6,21 @@ from domain.entities.Funcionario import Funcionario
 router = APIRouter()
 
 @router.get('/funcionario/', tags=['Funcionário'], status_code=200)
-def get_funcinario():
+async def get_funcinario():
     return {"msg": "funcinario get todos executado"}
 
 @router.get('/funcinario/{id}', tags=["Funcionário"], status_code=200)
-def get_funcinario(id: int):
+async def get_funcinario(id: int):
     return {"msg": "funciomario get um executado"}
 
 @router.post('/funcionario', tags=['Funcionário'], status_code=200)
-def post_funcionario(corpo: Funcionario):
+async def post_funcionario(corpo: Funcionario):
     return {"msg": "funcionario post executado", "nome": corpo.nome, "cpf": corpo.cpf, "telefone": corpo.telefone}
 
 @router.put('/funcionario/{id}', tags=['Funcionário'], status_code=200)
-def put_funcionario(id: int, corpo: Funcionario):
+async def put_funcionario(id: int, corpo: Funcionario):
     return {"msg": "funcionario put executado", "nome": corpo.nome, "cpf": corpo.cpf, "telefone": corpo.telefone}
 
 @router.delete('/funcionario/{id}', tags=['Funcionário'], status_code=200)
-def delete_funcionario(id: int):
+async def delete_funcionario(id: int):
     return {"msg": "funcionario delete executado", "id":id}
--- src/routers/ProdutoRouter.py
+++ src/routers/ProdutoRouter.py
@@ -6,21 +6,21 @@ from domain.entities.Produto import Produto
 router = APIRouter()
 
 @router.get('/produto', tags=['Produto'], status_code=200)
-def get_produto():
+async def get_produto():
     return {'msg': 'produto get todos executado'}
 
 @router.get('/produto/{id}', tags=['Produto'], status_code=200)
-def get_produto(id: int):
+async def get_produto(id: int):
     return {'msg': 'produto get um executado'}
 
 @router.post('/produto', tags=['Produto'], status_code=200)
-def post_produto(corpo: Produto):
+async def post_produto(corpo: Produto):
     return {'msg': 'produto post executado', "nome": corpo.nome, "descricao": corpo.descricao, "foto": corpo.foto, "valor_unitario": corpo.valor_unitario}
 
 @router.put('/produto/{id}', tags=['Produto'], status_code=200)
-def put_produto(id: int, corpo: Produto):
+async def put_produto(id: int, corpo: Produto):
     return {'msg': 'produto put executado', "nome": corpo.nome, "descricao": corpo.descricao, "foto": corpo.foto, "valor_unitario": corpo.valor_unitario}
 
 @router.delete('/produto/{id}', tags=['Produto'], status_code=200)
-def delete_produto(id: int):
+async def delete_produto(id: int):
     return {'msg': 'produto delete executado', "id":id}
